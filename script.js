d466375dc51c8623515b6c5766b2f7529b0d7e14
Shoaib Khan <shoaib7878687@gmail.com>
2026-03-27 10:02:54 +0500

    updated changes

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const img = document.querySelector(".img2");
+const img2 = document.querySelector(".img2");
 
-const observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach((entry) => {
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
     if (entry.isIntersecting) {
-      img.classList.add("animate");
-      observer.unobserve(img);
+
+      // reset animation
+      img2.style.animation = "none";
+
+      // force restart (important trick)
+      void img2.offsetHeight;
+
+      // re-apply same CSS animation
+      img2.style.animation = "landing 1.5s ease-out forwards";
     }
   });
+}, {
+  threshold: 0.5
 });
 
-observer.observe(img);
+observer.observe(img2);
