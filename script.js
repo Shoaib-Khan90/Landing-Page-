8db0d1bfab65cfec188f7071a675692137ad1304
Shoaib Khan <shoaib7878687@gmail.com>
2026-03-30 22:11:10 +0500

    updated changes

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 const img2 = document.querySelector(".img2");
 
-const observer = new IntersectionObserver((entries) => {
+const observer = new IntersectionObserver((entries, observer) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
 
-      // reset animation
-      img2.style.animation = "none";
+      img2.style.animation = "landing 1.5s cubic-bezier(0.25, 1, 0.5, 1) forwards";
 
-      // force restart (important trick)
-      void img2.offsetHeight;
-
-      // re-apply same CSS animation
-      img2.style.animation = "landing 1.5s ease-out forwards";
+      observer.unobserver(img2);
     }
   });
 }, {
-  threshold: 0.5
+  threshold: 0.6
 });
 
 observer.observe(img2);
+
+const img3 = document.querySelector(".img3");
+
+const observer3 = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+
+      img3.style.animation = "landing3 1.8s cubic-bezier(0.25, 1, 0.5, 1) forwards";
+
+      observer.unobserve(img3); // sirf 1 dafa
+    }
+  });
+}, {
+  threshold: 0.6
+});
+
+observer3.observe(img3);
